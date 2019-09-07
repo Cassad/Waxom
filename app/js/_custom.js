8276470e51a21c8b02fcbfacb5bc40c624b1af48
devine <druncker@gamil.com>
2019-09-07 12:51:26 +0300

    Добавил parallax к header, preloader, подключил wow, стилизовал главное меню

--- app/js/_custom.js
+++ app/js/_custom.js
@@ -1,5 +1,20 @@
 document.addEventListener("DOMContentLoaded", function() {
+//parallax
+$('.main-header').parallax();
+//preloader
+$(".loaderInner").fadeOut(500);
+$(".loader").delay(500).fadeOut("slow");
+//menu-click
+$(".navbar__menu-link").click(function(e){
+  e.preventDefault();
+  $(".navbar__menu-link").removeClass("navbar__menu-link--current");
+  $(this).addClass("navbar__menu-link--current");
+});
+//wow
+new WOW().init();
 
-	// Custom JS
 
 });
+
+
+
